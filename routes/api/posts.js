d4ca0ebaee33a4699b265ba7b56d88f384cfb5bb
hamdi7302 <hamdi.bntiba@gmail.com>
2018-10-26 11:51:37 +0100

    fix all model needed

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,49 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport')
 // @route   GET api/posts/test
 // @desc    Tests post route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
+
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/posts', (req, res) => res.json({ msg: 'Posts Works' }));
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/posts/:id', (req, res) => res.json({ msg: 'Posts Works' }));
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post('/posts', passport.authenticate("jwt", { session: false }),(req, res) => res.json({ msg: 'Posts Works' }));
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+router.delete('/posts/:id', (req, res) => res.json({ msg: 'Posts Works' }));
+
+// @route   POST api/posts/like/:id
+// @desc    Like post
+// @access  Private
+router.post('/posts/like/:id', (req, res) => res.json({ msg: 'Posts Works' }));
+
+// @route   POST api/posts/unlike/:id
+// @desc    Unlike post
+// @access  Private
+router.post('/posts/unlike/:id', (req, res) => res.json({ msg: 'Posts Works' }));
+
+// @route   POST api/posts/comment/:id
+// @desc    Add comment to post
+// @access  Private
+router.post('/posts/comment/:id', (req, res) => res.json({ msg: 'Posts Works' }));
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Remove comment from post
+// @access  Private
+router.get('/posts/comment/:id/:comment_id', (req, res) => res.json({ msg: 'Posts Works' }));
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const Profile = require("../../models/Profile");
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
-router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
+router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        return profile
+          ? res.json({ profile })
+          : res.json({ msg: "enable to fetch your profile" });
+      })
+      .catch(err => console.log(err));
+  }
+);
+
+// @route   POST api/profile
+// @desc    Create or edit user profile
+// @access  Private
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    newProfile = new Profile({
+      user: req.user.id,
+      handle: req.body.handle,
+      status: req.body.status
+    });
+    newProfile
+      .save()
+      .then(profile => res.json({ msg: "profile updated", profile }));
+  }
+);
 
 module.exports = router;
