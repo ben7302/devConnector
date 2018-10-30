8a86ca0d16b1ee01cbfc3e127fcf052d721910b2
hamdi7302 <hamdi.bntiba@gmail.com>
2018-10-30 14:48:46 +0100

    step 2

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,9 +1,10 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+ import React, { Component } from 'react'
+ class Footer extends Component {
+   render() {
+     return (
+      <footer className="bg-dark text-white mt-50 p-50 text-center">Copyright &copy; {new Date().getFullYear()}</footer>
+     )
+   }
+ }
+ 
+ export default Footer; 
